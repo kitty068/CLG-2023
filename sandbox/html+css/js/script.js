48820ed74f42184d391dbbe9b6462a65d34412ca
adel <ada.vyskocilova@gmail.com>
2023-07-19 16:15:42 +0200

    initial

---
+++ sandbox/html+css/js/script.js
@@ -0,0 +1,25 @@
+console.log("Hello world")
+let pokusu = 0;
+console.log(pokusu);
+function myfunction(p1, p2) {
+    return p1 * p2;   //The function returns the product of p1 and p2
+}
+let vysledek = myfunction(4, 3);
+console.log(vysledek);
+function ahoj(parametr) {
+    alert('Ahoj ' + parametr);
+}
+let polecisel = [1, 2, 3, 4, 5]
+let poletextu = Array("ahoj", "puf" ":)",)
+
+console.log(polecisel[0]); // = 1
+console.log(polecisel[2); // = 3
+console.log(poletextu[2); // = ;]
+
+//komplikovane :]
+let polepoli = new array([1, 2], ["uf", "nee"])
+console.log(poletextu[0][0]); // = 1
+
+
+
+
